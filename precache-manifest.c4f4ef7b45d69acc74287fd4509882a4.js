6a02e19c4c0a56e19e01ede806b5cd93392de03b
Nathan <nathan.froeh@gmail.com>
2019-07-11 19:40:26 -0600

    Updates

---
+++ precache-manifest.c4f4ef7b45d69acc74287fd4509882a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8af70704be0e582075cbc5e13321dc1",
+    "url": "/lightside/index.html"
+  },
+  {
+    "revision": "10fe4cbc3a714092321b",
+    "url": "/lightside/static/css/main.ef050a4b.chunk.css"
+  },
+  {
+    "revision": "aa4c8c27f516905ba58c",
+    "url": "/lightside/static/js/2.61bbfb1b.chunk.js"
+  },
+  {
+    "revision": "10fe4cbc3a714092321b",
+    "url": "/lightside/static/js/main.c2ce87a3.chunk.js"
+  },
+  {
+    "revision": "ecdc0bcbd4ae9fd720f2",
+    "url": "/lightside/static/js/runtime~main.30f30001.js"
+  },
+  {
+    "revision": "5c268a03897fb521fceb679626d5aefc",
+    "url": "/lightside/static/media/giphy.5c268a03.gif"
+  },
+  {
+    "revision": "8faa206d7897aa898c49a85a947f4812",
+    "url": "/lightside/static/media/green.8faa206d.png"
+  },
+  {
+    "revision": "18149f34d88c434b9a8b6e454200a129",
+    "url": "/lightside/static/media/red.18149f34.png"
+  },
+  {
+    "revision": "b041e58011180e16894fe15eb2c5f129",
+    "url": "/lightside/static/media/star-background.b041e580.jpg"
+  }
+]);
